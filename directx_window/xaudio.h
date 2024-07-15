45aa8094e9657c5a3b0f993e816cbca6408efba9
Salmaan Saeed <salsaeed999@gmail.com>
2024-07-15 14:58:48 -0400

    attempting to play the audio file but it is not working

---
+++ directx_window/xaudio.h
@@ -0,0 +1,23 @@
+//#pragma once
+
+/* Big - Endian */
+#ifndef _XBOX
+#define _XBOX
+#define fourccRIFF 'RIFF'
+#define fourccDATA 'data'
+#define fourccFMT 'fmt'
+#define fourccWAVE 'WAVE'
+#define fourccXWMA 'XMWA'
+#define foruccDPDS 'dpds'
+#endif
+
+/* Little-Endian */
+#ifndef _XBOX
+#define _XBOX
+#define fourccRIFF 'FFIR'
+#define forccDATA 'atad'
+#define fourccFMT 'tmf'
+#define fourccWAVE 'EVAW'
+#define fourccXWMA 'AMWX'
+#define foruccDPDS 'sdpd' 
+#endif
